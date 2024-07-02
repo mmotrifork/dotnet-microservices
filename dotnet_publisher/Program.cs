26e4926a8f853eeb82b2cc3134ca502992e92c73
Mads Kvist Møller <mmo@trifork.com>
2024-07-02 10:06:03 +0200

    Add dotnet producer and consumer

---
+++ dotnet_publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "rabbitmq" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+var message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: "",
+                     routingKey: "hello",
+                     basicProperties: null,
+                     body: body);
+
+Console.WriteLine($"[x] Sent {message}");
