0db0ceb18c80f505312638033d1094fab56fc4e2
Mads Kvist Møller <mmo@trifork.com>
2024-07-05 12:37:24 +0200

    Pass data base connection settings as environment variables

--- dotnet_consumer/Model/PayloadContext.cs
+++ dotnet_consumer/Model/PayloadContext.cs
@@ -9,8 +9,16 @@ namespace dotnet_consumer.Model
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             Console.WriteLine("Configuring PayloadContext");
-            // optionsBuilder.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=postgres");
-            optionsBuilder.UseNpgsql("Host=db.dotnet_consumer;Database=postgres;Username=postgres;Password=postgres");
+
+            // Construct the connection string using environment variables
+            var host = Environment.GetEnvironmentVariable("DB_HOST");
+            var database = Environment.GetEnvironmentVariable("DB_NAME");
+            var username = Environment.GetEnvironmentVariable("DB_USER");
+            var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+            var connectionString = $"Host={host};Database={database};Username={username};Password={password}";
+
+            optionsBuilder.UseNpgsql(connectionString);
         }
 
         internal async Task<IEnumerable<Payload>> GetAllPayloadsAsync()
